e8911811d869376696a5bc8c370c00bdb5252cec
何永能 <yongneng.he@dianping.com>
2016-05-10 16:33:26 +0800

    初始化

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { Page, Button } from 'm-hui';
+
+ReactDOM.render(
+    <Page title='HUI Mobile APP' spacing>Hello, world!</Page>,
+    document.getElementById('container')
+);
